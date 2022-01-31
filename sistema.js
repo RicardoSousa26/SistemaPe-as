01b06c9d11f6ff4bab5eca512bb7fa5443d97e6c
Ricardo Sousa26 <Ricardo.Sousamoraes@hotmail.com>
2022-01-31 19:39:23 -0300

    Sistema de peças

---
+++ sistema.js
@@ -0,0 +1,27 @@
+if  pesoPeca  =  100
+
+if ( pesoPeca  >  100 )  {
+ console . log ( "Peso maior que 100g, logo, podemos cadastrar a peça!!!" )
+}  else  {
+    console.log ( "Peso insuficiente, não é possível cadastrar." )
+}
+
+let  numeroPecas ;
+
+numeroPecas  =  10 ;
+
+
+if ( numeroPecas  <  10 )  {
+    console.log ( "Ainda há espaço na caixa, podemos cadastrar a peça" )
+}  else  {
+    console.log ( "Não há espaço disponível na caixa" )
+
+
+let  nomePeca  =  'Di'
+
+console.log ( "O cumprimento do nome da peça é:" ,  nomePeca . length )
+
+if ( nomePeca.comprimento  <  3 )  {
+    consola.log ( "Nome de peça muito curto, não é possível cadastrar" )
+}  else  {
+    console.log ( "Nome de peça adaptável, podemos cadastrar" )
